7cb277c5f3e32baaef60dc6682d1ecce24b8c496
Igor Trotskii <igortrotskii@protonmail.comi>
2022-08-08 22:32:52 +0300

    term strength calculations

---
+++ src/preprocessing/feature_extraction/text/filtering.py
@@ -0,0 +1,71 @@
+import pandas as pd 
+import numpy as np
+from typing import Union
+from scipy.sparse import csr_matrix, csc_matrix
+from scipy.special import comb
+
+class TermStrengthFeatureExtractor:
+    """
+    Calculates term strength according to http://mlwiki.org/index.php/Term_Strength
+    """
+    def __init__(self):
+        self.term_strength = {}
+        self.classes = []
+
+    def fit(self, X, y):
+        """
+        Fit feature extractor
+        Arguments:
+            X -  presense of word (binary output from CountVectorizer)
+            y - array-like with class labels for X
+        """
+        if isinstance(y, np.ndarray):
+            classes = np.unique() 
+        elif isinstance(y, (pd.Series, pd.DataFrame)):
+            classes = y.unique()
+        
+        self.classes = classes
+
+        for cls in classes:
+            idx = np.where(y == cls)[0] # might need [0] to unbox the tuple
+            X_cls = X[idx]
+            n_docs, _ = X_cls.shape
+
+            n_t = X_cls.sum(axis=0) # number of documents with term in question (each column for each term)
+            s_t = comb(n_t, 2) / (comb(n_docs, 2) - comb(n_docs-n_t, 2)) # N of pairs where t in both docs divided by number of pairs where t in at least one doc
+            s_t = np.nan_to_num(s_t) # set s_t to 0 if the word was not present for that class
+            self.term_strength[cls] = s_t
+    
+    def transform(self, X):
+        """
+        Transform word presense into term strength. Returns s_t for each class for each word.
+        Arguments:
+            X - binary output from CountVectorizer
+        Returns:
+            s_t - dict with a structure {class_label : transformed features as csr_matrix}
+        """
+        X = X.asfptype()
+        s_t = {}
+        for cls in self.classes:
+            s_t[cls] = X.minimum(np.tile(self.term_strength_full[cls], (X.shape[0], 1))) # sets term (0 or 1) to term strength, term strength is <= 1, so element wise min works
+        
+        return s_t 
+
+    def prune(self, n_std):
+        """
+        Prune weak terms. Prune term t if s(t) <= std(s)*n_std*E[s(t)]
+        Arguments:
+            n_std - pruning param
+        Returns:
+            keep_idx - dict {class_label:pruned_idx}, where prune_idx is an array with remaining indexes 
+        """
+        keep_idx = {}
+        for cls in self.classes:
+            E_t = np.mean(self.term_strength_full[cls])
+            std = np.std(self.term_strength_full[cls])
+            keep_idx[cls] = np.where(self.term_strength_full[cls] > n_std*std*E_t)
+
+        return keep_idx
+
+
+            
